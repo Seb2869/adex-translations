bf8b9248c1c38596b0cfa684e068cdb4f2d71006
Ivo paunov <ivo.paunov@gmail.com>
2018-03-02 11:39:14 +0200

    update non  EN lans keys

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // No automatic generation: we don't want to depend on fs, as we want to keep this universal
 // ls *-*.json | while read line; do printf "\"`printf $line | cut -d '.' -f1`\": require(\"./$line\"),\n"; done
-module.exports = function() { 
+module.exports = function () {
 	return {
 		"bg-BG": require("./bg-BG.json"),
 		"de-DE": require("./de-DE.json"),
@@ -39,4 +39,9 @@ module.exports.all = [
 	"se-SE",
 	"sr-RS",
 	"zh-CN",
-];
+]
+
+module.exports.onlyTranslated = [
+	"en-US",
+	"es-ES"
+]
